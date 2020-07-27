896620dc067f95b3d58663c3009214ae7f668785
tanshuangxi <shuangxi.tan@zhiqiutech.com>
2020-07-27 15:59:27 +0800

    自定义微信小程序第三方组件

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,62 +1,55 @@
-import { xComponent } from '../../lib/wrapper/index'
-const time=require('../../lib/wrapper/dayjs/index')
+import {
+  xComponent
+} from '../../lib/wrapper/index'
+const time = require('../../lib/wrapper/dayjs/index')
 
 xComponent({
   props: {
-   
+
   },
   data: {
-    result:0
+    result: 0
   },
 
-  created(){
-  },
-  mounted(){
-    console.log('time',new time().$d)
-    this.$getRect('.texts').then(res=>{
+  created() {},
+  mounted() {
+    console.log('time', new time().$d)
+    this.$getRect('.texts').then(res => {
       console.log(res.height)
     })
-    
-    this.$addEventListener('HHHHH',()=>
+
+    this.$addEventListener('HHHHH', () =>
       this.aaa()
     )
-    console.log('username',this.data.username)
-    
-    // var myAnimation = anime({
-    //   targets: '.circle',
-    //   translateX: '13rem',
-    //   rotate: 180,
-    //   borderRadius: '50%',
-    //   duration: 6000,
-    //   backgroundColor: '#fbc531',
-    //   loop: true
-    // })
+    console.log('username', this.data.username)
+
+
   },
-  watch:{
-    'username':function(username){
-      console.log('转变后的username',username)
+  watch: {
+    'username': function (username) {
+      console.log('转变后的username', username)
     }
   },
-  methods:{
-    buttonTap(){
+  methods: {
+    buttonTap() {
       this.update('张萍')
     },
-   
-    aaa(){
+
+    aaa() {
       console.log('监听了，哈哈哈')
     },
 
-    handle(){
+    handle() {
       console.log('点击了')
       // debugger
       this.$dispatch('HHHHH')
       this.$set({
-        result:'这是结果啦'
-      }).then(res=>{
-        this.$setItem('ok','成功了')
+        result: '这是结果啦'
+      }).then(res => {
+        this.$setItem('ok', '成功了')
         console.log('渲染成功！')
       })
     }
   }
-  
+
 });
